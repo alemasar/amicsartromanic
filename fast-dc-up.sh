fd2a655041fdd31f1fa05f9a8d2adecd0fb5f8f9
Alemasar <you@example.com>
2018-12-20 16:55:58 +0100

    First working version with webpack

--- fast-dc-up.sh
+++ fast-dc-up.sh
@@ -2,7 +2,7 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 printf "${GREEN}Installing website dependencies${NC}\n"
-cd app
+cd amicsartromanic/app
 if test -d node_modules; 
 then
   printf "${GREEN}website node_modules exists, skipping npm install${NC}\n"
